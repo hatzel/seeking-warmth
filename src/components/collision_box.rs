da3634de34fe290be97ec3b4c72b1e82e19a5565
Hans Ole Hatzel <hhatzel@gmail.com>
2019-01-26 21:47:16 +0100

    Collision boxes

---
+++ src/components/collision_box.rs
@@ -0,0 +1,32 @@
+use amethyst::ecs::prelude::*;
+use amethyst::core::Transform;
+
+pub struct AABB {
+    pub width: f32,
+    pub height: f32,
+}
+
+impl AABB {
+    fn new(width: f32, height: f32) -> Self {
+        Self {
+            width,
+            height,
+        }
+    }
+
+    fn collides(&self, transform: &Transform, other: &Self, other_transform: &Transform) -> bool {
+        let delta_x = (transform.translation().x - other_transform.translation().x).abs();
+        let delta_y = (transform.translation().y - other_transform.translation().y).abs();
+        if (delta_x <= (self.height + other.height) * 0.5) {
+            true
+        } else if (delta_y <= (self.width + other.width) * 0.5) {
+            true
+        } else {
+            false
+        }
+    }
+}
+
+impl Component for AABB {
+    type Storage = DenseVecStorage<Self>;
+}
--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,5 +1,6 @@
 pub mod player;
 pub mod movement;
+pub mod collision_box;
 
 pub use crate::components::player::Player;
 pub use crate::components::movement::Movement;
